2bc6a8d7b78d00c50a15646f3d8bd1c27abf3b7f
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-19 00:32:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
@@ -17,3 +16,31 @@ export default function App() {
 
   const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
+
+  useEffect(() => {
+    const page =  searchParams.get("page") ?? 1;
+
+    if(location.pathname.includes("tags")) {
+      //it means tag page will show 
+      const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page), tag);
+    }
+    // this means category page will show
+    else if(location.pathname.includes("categories")) {
+      const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page), null, category);
+    }
+    else {
+      fetchBlogPosts(Number(page));
+    }
+  }, [location.pathname, location.search]);
+
+  return (
+    <Routes>
+      <Route path="/" element = {<Home/>}   />
+      <Route path="/blog/:blogId" element = {<BlogPage/>}   />
+      <Route path="/tags/:tag" element = {<TagPage/>}   />
+      <Route path="/categories/:category" element = {<CategoryPage/>}   />
+    </Routes>
+  );
+}
