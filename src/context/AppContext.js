8a0e97d38ecaad40bef620896324b3b853226a76
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-21 00:13:21 +0600

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -12,3 +12,35 @@ export default function AppContextProvider({ children }) {
   const navigate = useNavigate();
 
   // Fetch Blog Data
+  const fetchBlogPosts = async (page = 1, tag=null, category) => {
+    setLoading(true);
+    let url = `${baseUrl}?page=${page}`;
+    if(tag) {
+      url += `&tag=${tag}`;
+    }
+    if(category) {
+      url += `&category=${category}`;
+    }
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      if (!data.posts || data.posts.length === 0)
+        throw new Error("Something Went Wrong");
+      console.log("Api Response", data);
+      setPage(data.page);
+      setPosts(data.posts);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.log("Error in Fetching BlogPosts", error);
+      setPage(1);
+      setPosts([]);
+      setTotalPages(null);
+    }
+    setLoading(false);
+  };
+
+  // Handle When Next and Previous button are clicked
+  const handlePageChange = (page) => {
+    navigate( { search: `?page=${page}`});
+    setPage(page);
+  };
