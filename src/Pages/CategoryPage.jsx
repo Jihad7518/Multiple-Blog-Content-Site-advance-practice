25942b91b3fe7ae0d27ebcd5fcfcd9dbfb8ac9be
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:03:40 +0600

    Update CategoryPage.jsx

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react'
+import Header from '../components/Header'
+import { useLocation, useNavigate } from 'react-router-dom';
+import Pagination from '../components/Pagination';
+import Blogs from '../components/Blogs';
+
+const CategoryPage = () => {
+
+    const navigation = useNavigate();
+    const location = useLocation();
+    const category = location.pathname.split("/").at(-1);
