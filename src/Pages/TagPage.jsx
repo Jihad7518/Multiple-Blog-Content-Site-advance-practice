67edab7b23b13dddbe2cbd115e06776af8348389
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:02:50 +0600

    Update TagPage.jsx

--- src/Pages/TagPage.jsx
+++ src/Pages/TagPage.jsx
@@ -1,1 +1,13 @@
 
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Pagination from '../components/Pagination';
+import Blogs from '../components/Blogs';
+
+
+const TagPage = () => {
+
+    const navigation = useNavigate();
+    const location = useLocation();
+    const tag = location.pathname.split("/").at(-1);
