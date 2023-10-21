2b06a1fe2add2d692d786fc5b6a0a6b55ddc0427
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:01:53 +0600

    Update BlogDetails.jsx

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
@@ -16,3 +15,17 @@ const BlogDetails = ({post}) => {
             <span>{post.category}</span>
         </NavLink>
       </p>
+      <p> Posted on {post.date} </p>
+      <p> {post.content}</p>
+      <div>
+        {post.tags.map( (tag, index) => (
+            <NavLink key={index} to={`/tags/${tag.replaceAll(" ","-")}`}>
+                <span>{`#${tag}`}</span>
+            </NavLink>
+        ) )}
+      </div>
+    </div>
+  )
+}
+
+export default BlogDetails
