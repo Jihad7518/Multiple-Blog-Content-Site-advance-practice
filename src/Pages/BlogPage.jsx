b7a6232c2458669aeb3845255844fc9224c50529
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:04:37 +0600

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -42,3 +42,39 @@ const BlogPage = () => {
             fetchRelatedBlogs();
         }
     }, [location.pathname] )
+  return (
+    <div>
+      <Header/>
+      <div>
+        <button
+        onClick={() => navigation(-1)}
+        >
+            Back
+        </button>
+      </div>
+      {
+        loading ?
+        (<div>
+            <p> Loading</p>
+        </div>) :
+        blog ?
+        (<div>
+            <BlogDetails post={blog} />
+            <h2> Related Blogs </h2>
+            {
+                relatedblogs.map( (post) => (
+                    <div key = {post.id}>
+                        <BlogDetails post={post} />
+                    </div>
+                ) )
+            }
+
+        </div>) :
+        (<div>
+            <p>No Blog Found</p>
+        </div>)
+       
+      }
+
+
+    </div>
