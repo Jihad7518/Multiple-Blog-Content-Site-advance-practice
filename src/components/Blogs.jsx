af2b0675d2df8536681daf9b6edf0175e1b5c8f7
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:01:12 +0600

    Update Blogs.jsx

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,1 +1,7 @@
 
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import BlogDetails from "./BlogDetails";
+
+export default function Blogs() {
+  const { posts, loading } = useContext(AppContext);
