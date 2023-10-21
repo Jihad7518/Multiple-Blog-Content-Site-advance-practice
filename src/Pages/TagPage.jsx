d4addb829db690e48522c97d32a91c3642835d5c
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:02:59 +0600

    Update TagPage.jsx

--- src/Pages/TagPage.jsx
+++ src/Pages/TagPage.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
@@ -11,3 +10,24 @@ const TagPage = () => {
     const navigation = useNavigate();
     const location = useLocation();
     const tag = location.pathname.split("/").at(-1);
+  return (
+    <div>
+        <Header/>
+        <div>
+            <button 
+            onClick={() => navigation(-1)}
+            >
+                back
+            </button>
+            <h2>
+                Blogs Tagged <span>#{tag}</span>
+            </h2>
+        </div>
+        <Blogs/>
+        <Pagination/>
+      
+    </div>
+  )
+}
+
+export default TagPage
