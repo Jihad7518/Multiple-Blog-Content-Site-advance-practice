4e977940cbf4f7c0edea706827901eaf4b65ea64
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:04:11 +0600

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -1,1 +1,19 @@
 
+import React, { useContext } from 'react'
+import { useNavigate, useNavigation } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from '../components/Header';
+import BlogDetails from '../components/BlogDetails';
+import { baseUrl } from '../baseUrl';
+
+const BlogPage = () => {
+    const newBaseUrl = "https://codehelp-apis.vercel.app/api/";
+    const [blog, setBlog] = useState(null);
+    const[relatedblogs, setRelatedBlogs] = useState([]);
+    const location = useLocation();
+    const navigation = useNavigate();
+    const {setLoading, loading} = useContext(AppContext);
+
+    const blogId = location.pathname.split("/").at(-1);
