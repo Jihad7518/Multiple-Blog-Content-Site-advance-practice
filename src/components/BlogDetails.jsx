54b2ae436df55e8d42ebbc47ea6e49745ebc3e9d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-22 01:01:44 +0600

    Update BlogDetails.jsx

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,1 +1,18 @@
 
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const BlogDetails = ({post}) => {
+  return (
+    <div className='mt-[50px]'>
+      <NavLink to={`/blog/${post.id}`} >
+        <span>{post.title}</span>
+      </NavLink>
+      <p>
+        By
+        <span>{post.author}</span>
+        on {" "}
+        <NavLink to={`/categories/${post.category.replaceAll(" ","-")}`}>
+            <span>{post.category}</span>
+        </NavLink>
+      </p>
